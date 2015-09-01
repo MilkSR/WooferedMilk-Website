cb2273a1b4068e1983bbffd84327724d0eb907d7
PowderedMilk <willwolf3@gmail.com>
2015-09-01 17:37:41 -0400

    messed with schema, added tables
    ZreknarF

--- schema.sql
+++ schema.sql
@@ -3,12 +3,13 @@ create table users (
   id INTEGER primary key autoincrement,
   name TEXT not null,
   status TEXT,
-  commands TEXT,
-  emotes TEXT,
-  pcommands TEXT,
+  twitch-title TEXT,
+  twitch-game TEXT,
+  emote-method TEXT,
   dogfacts INTEGER not null,
   dogs INTEGER not null,
   multi INTEGER not null,
+  novelty INTEGER not null,
   quote INTEGER not null,
   speedrun INTEGER not null,
   utility INTEGER not null,
@@ -21,3 +22,21 @@ create table users (
   quotes TEXT,
   trigger TEXT not null
 );
+create table commands (
+  id INTEGER primary key autoincrement,
+  command-name TEXT not null,
+  command-response TEXT not null,
+  channel-id Foreign Key INTEGER
+)
+create table personal-commands (
+  id INTEGER primary key autoincrement,
+  command-name TEXT not null,
+  command-response TEXT not null,
+  user-id Foreign Key INTEGER
+)
+create table emote-list (
+  id INTEGER primary key autoincrement,
+  emote-name TEXT not null.
+  emote-use INTEGER not null.
+  channel-id Foreign Key INTEGER
+)
